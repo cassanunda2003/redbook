966ebf7c669ac4ce940aa1c63b9c95a19d4cd441
Mark Reynolds <mark.reynolds2@gmail.com>
2018-11-10 12:28:46 +0000

    Further streams

--- src/main/scala/laziness/Stream.scala
+++ src/main/scala/laziness/Stream.scala
@@ -1,11 +1,24 @@
 package laziness
 
+import Stream._
+
 sealed trait Stream[+A] {
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
   }
-
+  //Book code sometimes uses mutable objects within local functions
+  def toListFast: List[A] = {
+    val buf = new collection.mutable.ListBuffer[A]
+    @annotation.tailrec
+    def go(s: Stream[A]): List[A] = s match {
+      case Cons(h,t) =>
+        buf += h()
+        go(t())
+      case _ => buf.toList
+    }
+    go(this)
+  }
 
   def toList: List[A] ={
       def innerList(acc: List[A], st: Stream[A]): List[A] = {
@@ -16,12 +29,43 @@ sealed trait Stream[+A] {
       }
     innerList(Nil, this).reverse
   }
+
+  def drop(n: Int): Stream[A] = {
+    def innerStream(x: Int, s: Stream[A]): Stream[A] = {
+      s match {
+        case Empty => Empty
+        case (Cons(h,t)) if x <= 0 => Cons(h,t)
+        case (Cons(h,t)) if x > 0 => innerStream(x-1,t())
+      }
+    }
+    innerStream(n,this)
+  }
+  //Fails as the stream is reverse stream but the values are correct
+  def takeMyImpl(n: Int): Stream[A] = {
+    def inner(x: Int, is: Stream[A], ps: Stream[A]): Stream[A] = {
+      ps match  {
+        case Empty => is
+        case (Cons(h,t)) if x == 0 => is
+        case (Cons(h,t)) if x > 0 => inner(x-1,Cons(h,() => is),t())
+      }
+    }
+    inner(n,Empty,this)
+  }
+
+  //Recursive had trouble as to why I would meed the import statement but it is necessary to include the methods
+  //in the object
+  def take(n: Int): Stream[A] = this match {
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
+  }
+
 }
 
 case object Empty extends Stream[Nothing]
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
-object Stream {
+object Stream  {
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
@@ -31,5 +75,6 @@ object Stream {
 
   def apply[A](as: A*): Stream[A] =
     if(as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+
 
 }
--- src/test/scala/laziness/StreamTest.scala
+++ src/test/scala/laziness/StreamTest.scala
@@ -4,9 +4,28 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class StreamTest extends FreeSpec with Matchers {
   "Stream" - {
+    val st : Stream[Int] = Stream(1,2,3,4)
     "Should give a list when using to list" in {
-      val st : Stream[Int] = Stream(1,2,3,4)
       st.toList should be (List(1,2,3,4))
+    }
+    "take should produce a Stream of the number of the argument" in {
+      st.take(2).toList should be (List(1,2))
+    }
+    "take 0 should return the empty Stream" in {
+      st.take(0).toList should be (List.empty)
+    }
+    "take with a number greater than than size should return the same stream " in{
+      st.take(5).toList should be (List(1,2,3,4))
+    }
+
+    "drop should return elements after the position given" in {
+      st.drop(2).toList should be (List(3,4))
+    }
+    "drop 0 return the same stream" in {
+      st.drop(0).toList should be (List(1,2,3,4))
+    }
+    "drop with more elements should return empty stream" in {
+      st.drop(5).toList should be (List.empty)
     }
   }
 
