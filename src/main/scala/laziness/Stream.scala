acb9b27c0a593e277a185e8f2afc9e59877d9c63
Mark Reynolds <mark.reynolds2@gmail.com>
2018-11-18 14:08:56 +0000

    Exercise 5.11 and 5.12

--- src/main/scala/laziness/Stream.scala
+++ src/main/scala/laziness/Stream.scala
@@ -5,30 +5,35 @@ import Stream._
 sealed trait Stream[+A] {
 
   val i1 = 1 + 1
+
   def headOption: Option[A] = this match {
     case Empty => None
     case Cons(h, t) => Some(h())
   }
+
   //Book code sometimes uses mutable objects within local functions
   def toListFast: List[A] = {
     val buf = new collection.mutable.ListBuffer[A]
+
     @annotation.tailrec
     def go(s: Stream[A]): List[A] = s match {
-      case Cons(h,t) =>
+      case Cons(h, t) =>
         buf += h()
         go(t())
       case _ => buf.toList
     }
+
     go(this)
   }
 
-  def toList: List[A] ={
-      def innerList(acc: List[A], st: Stream[A]): List[A] = {
-        st match {
-          case Empty => acc
-          case Cons(h, t) => innerList(h()::acc, t())
-        }
+  def toList: List[A] = {
+    def innerList(acc: List[A], st: Stream[A]): List[A] = {
+      st match {
+        case Empty => acc
+        case Cons(h, t) => innerList(h() :: acc, t())
       }
+    }
+
     innerList(Nil, this).reverse
   }
 
@@ -36,22 +41,25 @@ sealed trait Stream[+A] {
     def innerStream(x: Int, s: Stream[A]): Stream[A] = {
       s match {
         case Empty => Empty
-        case (Cons(h,t)) if x <= 0 => Cons(h,t)
-        case (Cons(h,t)) if x > 0 => innerStream(x-1,t())
+        case (Cons(h, t)) if x <= 0 => Cons(h, t)
+        case (Cons(h, t)) if x > 0 => innerStream(x - 1, t())
       }
     }
-    innerStream(n,this)
+
+    innerStream(n, this)
   }
+
   //Fails as the stream is reverse stream but the values are correct
   def takeMyImpl(n: Int): Stream[A] = {
     def inner(x: Int, is: Stream[A], ps: Stream[A]): Stream[A] = {
-      ps match  {
+      ps match {
         case Empty => is
-        case (Cons(h,t)) if x == 0 => is
-        case (Cons(h,t)) if x > 0 => inner(x-1,Cons(h,() => is),t())
+        case (Cons(h, t)) if x == 0 => is
+        case (Cons(h, t)) if x > 0 => inner(x - 1, Cons(h, () => is), t())
       }
     }
-    inner(n,Empty,this)
+
+    inner(n, Empty, this)
   }
 
   //Recursive had trouble as to why I would meed the import statement but it is necessary to include the methods
@@ -62,12 +70,12 @@ sealed trait Stream[+A] {
     case _ => empty
   }
 
-  def takeWhile(f: A => Boolean):Stream[A] = this match {
-    case Cons(h,t) if(f(h())) => cons(h(), t() takeWhile(f))
+  def takeWhile(f: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) if (f(h())) => cons(h(), t() takeWhile (f))
     case _ => Empty
   }
 
-  def takeWhileRight(f: A => Boolean):Stream[A] = foldRight(Empty: Stream[A])((a, b) => if(f(a)) cons(a, b) else empty)
+  def takeWhileRight(f: A => Boolean): Stream[A] = foldRight(Empty: Stream[A])((a, b) => if (f(a)) cons(a, b) else empty)
 
   def existsRec(p: A => Boolean): Boolean = this match {
     case Cons(h, t) => p(h()) || t().exists(p)
@@ -80,11 +88,10 @@ sealed trait Stream[+A] {
     foldRight(false)((a, b) => p(a) || b)
 
   def foldRight[B](z: => B)(f: (A, => B) => B): B =
-      this match {
-         case Cons(h,t) => f(h(), t().foldRight(z)(f))
-          case _ => z
+    this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
     }
-
 
 
   def forAll(p: A => Boolean): Boolean = foldRight(true)((a, b) => p(a) && b)
@@ -95,17 +102,15 @@ sealed trait Stream[+A] {
 
   def map[B](f: A => B): Stream[B] = foldRight(Empty: Stream[B])((a, b) => cons(f(a), b))
 
-  def filter(f: A => Boolean): Stream[A] = foldRight(Empty: Stream[A])((a, b) => if(f(a)) cons(a,b) else b)
+  def filter(f: A => Boolean): Stream[A] = foldRight(Empty: Stream[A])((a, b) => if (f(a)) cons(a, b) else b)
 
-  def append[B>:A](s1: Stream[B]): Stream[B] = foldRight(s1)((x,b) => cons(x,b))
+  def append[B >: A](s1: Stream[B]): Stream[B] = foldRight(s1)((x, b) => cons(x, b))
 
-  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Empty: Stream[B])((a,b) =>f(a) append b)
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Empty: Stream[B])((a, b) => f(a) append b)
 
-  def constant[A](a: A): Stream[A] = cons(a,constant(a))
+  def constant[A](a: A): Stream[A] = cons(a, constant(a))
 
-  def from(i: Int): Stream[Int] = cons(i,from(i+1))
-
-  //Book solutions
+  def from(i: Int): Stream[Int] = cons(i, from(i + 1))
 
   // This is more efficient than `cons(a, constant(a))` since it's just
   // one object referencing itself.
@@ -114,30 +119,50 @@ sealed trait Stream[+A] {
     tail
   }
 
+  //Book solutions
+
   def fromBook(n: Int): Stream[Int] =
-    cons(n, from(n+1))
+    cons(n, from(n + 1))
 
   def fibs: Stream[Int] = {
-    def fib(a: Int,b: Int): Stream[Int] = cons(a,fib(b,a+b))
-    fib(0,1)
+    def fib(a: Int, b: Int): Stream[Int] = cons(a, fib(b, a + b))
+
+    fib(0, 1)
   }
+
+  //This was fairly easy now
+  def unFold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] = f(z) match {
+    case None => empty
+    case Some((a, b)) => cons(a, unFold(b)(f))
+  }
+
+  //failed miserably so this is the book version
+  //Basically I didn't consider that S could be a tuple
+  def fibsUnfold: Stream[Int] = unFold((0,1)) { case (f0, f1) => Some((f0,(f1, f0+f1)))}
+
+  def fromUnfold(n: Int): Stream[Int] = unFold(n) {case x => Some(x, x+1)}
+
+  def constantUnfold[A](a: A): Stream[A] = unFold(a) {case x => Some(x, x)}
+
+  def onesUnfold: Stream[Int] = unFold(1) {(x) => Some((x, x))}
 }
 
 case object Empty extends Stream[Nothing]
+
 case class Cons[+A](h: () => A, t: () => Stream[A]) extends Stream[A]
 
-object Stream  {
+object Stream {
 
   def cons[A](hd: => A, tl: => Stream[A]): Stream[A] = {
     lazy val head = hd
     lazy val tail = tl
-    Cons(()=>head, () => tail)
+    Cons(() => head, () => tail)
   }
 
   def empty[A]: Stream[A] = Empty
 
   def apply[A](as: A*): Stream[A] =
-    if(as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
+    if (as.isEmpty) empty else cons(as.head, apply(as.tail: _*))
 
 
 }
--- src/test/scala/laziness/StreamTest.scala
+++ src/test/scala/laziness/StreamTest.scala
@@ -4,82 +4,94 @@ import org.scalatest.{FreeSpec, Matchers}
 
 class StreamTest extends FreeSpec with Matchers {
   "Stream" - {
-    val st : Stream[Int] = Stream(1,2,3,4)
+    val st: Stream[Int] = Stream(1, 2, 3, 4)
     "Should give a list when using to list" in {
-      st.toList should be (List(1,2,3,4))
+      st.toList should be(List(1, 2, 3, 4))
     }
     "take should produce a Stream of the number of the argument" in {
-      st.take(2).toList should be (List(1,2))
+      st.take(2).toList should be(List(1, 2))
     }
     "take 0 should return the empty Stream" in {
-      st.take(0).toList should be (List.empty)
+      st.take(0).toList should be(List.empty)
     }
-    "take with a number greater than than size should return the same stream " in{
-      st.take(5).toList should be (List(1,2,3,4))
+    "take with a number greater than than size should return the same stream " in {
+      st.take(5).toList should be(List(1, 2, 3, 4))
     }
 
     "drop should return elements after the position given" in {
-      st.drop(2).toList should be (List(3,4))
+      st.drop(2).toList should be(List(3, 4))
     }
     "drop 0 return the same stream" in {
-      st.drop(0).toList should be (List(1,2,3,4))
+      st.drop(0).toList should be(List(1, 2, 3, 4))
     }
     "drop with more elements should return empty stream" in {
-      st.drop(5).toList should be (List.empty)
+      st.drop(5).toList should be(List.empty)
     }
     "takewhile should return stream containing values less than 3" in {
-      val st = Stream(1,2,3,4,5,1)
-      st.takeWhile(x => x < 3).toList should be (List(1,2))
+      val st = Stream(1, 2, 3, 4, 5, 1)
+      st.takeWhile(x => x < 3).toList should be(List(1, 2))
     }
     "takewhileRight should return stream containing values less than 3" in {
-      st.takeWhileRight(x => x < 3).toList should be (List(1,2))
+      st.takeWhileRight(x => x < 3).toList should be(List(1, 2))
     }
 
     "takewhileRight should return stream containing values less than 3 but not those after 5" in {
-      val st = Stream(1,2,3,4,5,1)
-      st.takeWhileRight(x => x < 3).toList should be (List(1,2))
+      val st = Stream(1, 2, 3, 4, 5, 1)
+      st.takeWhileRight(x => x < 3).toList should be(List(1, 2))
     }
     "exists should return true" in {
-      st.exists(_ == 2) should be (true)
+      st.exists(_ == 2) should be(true)
     }
     "exists should return false" in {
-      st.exists(_ == 9) should be (false)
+      st.exists(_ == 9) should be(false)
     }
     "forall should return true" in {
-      st.forAll(_ < 9) should be (true)
+      st.forAll(_ < 9) should be(true)
     }
     "forall should return false" in {
-      st.forAll(_ < 2) should be (false)
+      st.forAll(_ < 2) should be(false)
     }
 
     "headOptionRight should return Some(1)" in {
-      st.headOptionRight should be (Some(1))
+      st.headOptionRight should be(Some(1))
     }
 
     "map should add 1 to each item in stream" in {
-      st.map((x) => x + 1).toList should be (List(2,3,4,5))
+      st.map((x) => x + 1).toList should be(List(2, 3, 4, 5))
     }
 
     "filter should return filtered stream" in {
-      st.filter(x => x%2 == 0).toList should be (List(2,4))
+      st.filter(x => x % 2 == 0).toList should be(List(2, 4))
     }
 
     "append should append new stream" in {
-      st.append(Stream(5,6)).toList should be (List(1,2,3,4,5,6))
+      st.append(Stream(5, 6)).toList should be(List(1, 2, 3, 4, 5, 6))
     }
 
     "flatMap" in {
-      st.flatMap(i => Stream(i,i)).toList should be (List(1,1,2,2,3,3,4,4))
+      st.flatMap(i => Stream(i, i)).toList should be(List(1, 1, 2, 2, 3, 3, 4, 4))
     }
 
     "constant" in {
-      st.constant(1).take(5).toList should be (List(1,1,1,1,1))
+      st.constant(1).take(5).toList should be(List(1, 1, 1, 1, 1))
     }
     "from" in {
-      st.from(3).take(5).toList should be (List(3,4,5,6,7))
+      st.from(3).take(5).toList should be(List(3, 4, 5, 6, 7))
+    }
+    "fromUnfold" in {
+      st.fromUnfold(3).take(5).toList should be(List(3, 4, 5, 6, 7))
     }
     "fib" in {
-      st.fibs.take(7).toList should be (List(0, 1, 1, 2, 3, 5, 8))
+      st.fibs.take(7).toList should be(List(0, 1, 1, 2, 3, 5, 8))
+    }
+    "fibs unfold" in {
+      st.fibsUnfold.take(7).toList should be(List(0, 1, 1, 2, 3, 5, 8))
+    }
+    "constantUnfold" in {
+      st.constantUnfold(1).take(5).toList should be(List(1, 1, 1, 1, 1))
+    }
+    "onesUnfold" in {
+      st.onesUnfold.take(5).toList should be(List(1, 1, 1, 1, 1))
     }
   }
 
