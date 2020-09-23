e9923c1cc9ede12e238428f599f5b54da08e8869
Mark <mark.reynolds2@gmail.co.uk>
2020-09-23 16:06:16 +0100

    State work

--- src/main/scala/state/SimpleRNG.scala
+++ src/main/scala/state/SimpleRNG.scala
@@ -1,11 +1,17 @@
 package state
 
+import java.awt.RadialGradientPaint
+
+
+
 trait RNG {
+
+      type Rand[+A] = RNG => (A, RNG)
   def nextInt: (Int, RNG)
   def nonNegativeInt(rng: RNG): ( Int, RNG)
 }
 
-case class SimpleRNG(seed: Long) extends RNG {
+class SimpleRNG(seed: Long) extends RNG {
   def nextInt:(Int, RNG) = {
     val newSeed = (seed * 0x5DEECE66DL + 0xBL) & 0xFFFFFFFFFFFFL
     val nextRNG = SimpleRNG(newSeed)
@@ -20,6 +26,109 @@ case class SimpleRNG(seed: Long) extends RNG {
         case(x, r) => (x, r)
     }
 
+  def double(rng: RNG): (Double, RNG) = {
+    val (i, r) = nonNegativeInt(rng)
+    (i / (Int.MaxValue.toDouble + 1), r)
+  }
+
+
+
+  def intDouble(rng: RNG): ((Int, Double), RNG) = {
+    val (i, rng2) = nonNegativeInt(rng)
+    val (d, rng3) = double(rng2)
+    ((i,d),rng3)
+  }
+
+  def doubleInt(rng: RNG): ((Double, Int), RNG) = {
+    val (d, rng2) = double(rng)
+    val (i, rng3) = nonNegativeInt(rng2)
+    ((d,i), rng3)
+  }
+
+  def double3(rng: RNG): ((Double, Double, Double), RNG) = {
+    val (d, rng2) = double(rng)
+    val (d2, rng3) = double(rng2)
+    val (d3, rng4) = double(rng3)
+    ((d,d2,d3), rng4)
+  }
+
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    def genList(count: Int, acc: List[Int], rng: RNG): (List[Int], RNG) = 
+      if (count <= 0) (acc, rng)
+      else {
+        val (i, rng2) = nonNegativeInt(rng)
+        genList(count -1, i :: acc, rng2)
+      }
+      genList(count, List(), rng)
+    }
+
+    val int: Rand[Int] = _.nextInt
+
+    def unit[A](a: A): Rand[A] = rng => (a, rng)
+
+    def map[A, B](s: Rand[A])(f: A => B): Rand[B] =
+      rng => {
+        val (a, rng2) = s(rng)
+        (f(a), rng2)
+      }
+    
+    def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = 
+        rng => {
+          val (a, rng2) = f(rng)
+          g(a)(rng2)
+        }
+   
+    def fmap[A, B](s: Rand[A])(f: A => B): Rand[B] = 
+        flatMap(s)(x => unit(f(x)))
+
+    def fmap2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+      flatMap(ra) { b => 
+        flatMap(rb) {
+           x => unit(f(b, x))
+        }
+      }
+    }
+
+
+    def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = {
+        rng => {
+          val (a, rng2) = ra(rng)
+          val (b, rng3) = rb(rng2)
+          (f(a, b), rng3)
+      }
+    }
+
+  val doubleElegant: Rand[Double] = 
+      map(nonNegativeInt)( i => i / (Int.MaxValue.toDouble + 1))
+
+   
+  val doubleElegant2: Rand[Double] = 
+      fmap(nonNegativeInt)( i => i / (Int.MaxValue.toDouble + 1))   
+
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = 
+    map2(ra, rb)((_, _))
+
+  def both2[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = 
+    fmap2(ra, rb)((_, _))
 
   
+  val randIntDouble: Rand[(Int, Double)] = both(int, doubleElegant)
+
+  val randIntDouble2: Rand[(Int, Double)] = both2(int, doubleElegant)
+
+
+  def nonNegativeEven: Rand[Int] = map(nonNegativeInt)(i => i - i % 2)
+
+
+  val randDoubleInt: Rand[(Double, Int)] =
+    both(double, int)
+
+
+
+}
+
+object SimpleRNG {
+
+    def apply(seed: Long) = new SimpleRNG(seed)
+
 }
--- src/main/scala/state/StatePlay.worksheet.sc
+++ src/main/scala/state/StatePlay.worksheet.sc
@@ -1,9 +1,57 @@
-val rng = new scala.util.Random
+// val rng = new scala.util.Random
 
-val r = rng.nextDouble
+// val r = rng.nextDouble
 
-val r2 = rng.nextDouble
+// val r2 = rng.nextDouble
 
-val r3 = rng.nextInt
+// val r3 = rng.nextInt
 
-val r4 = rng.nextInt(10)
+// val r4 = rng.nextInt(10)
+
+import state._
+
+val rng = SimpleRNG(42)
+
+val (i, rng2) = rng.nextInt
+
+val (i2, rng3) = rng2.nextInt
+
+def wrongRandomPair(rng: RNG): (Int, Int) = {
+    val (i1, _) = rng.nextInt
+    val (i2, _) = rng.nextInt
+    (i1, i2)
+} //Returnd the same value
+
+wrongRandomPair(rng)
+
+def rightRandomPair(rng: RNG): ((Int, Int), RNG) = {
+    val (i1, rng2) = rng.nextInt
+    val (i2, rng3) = rng2.nextInt
+    ((i1, i2), rng3)
+}
+
+rightRandomPair(rng)
+
+rng.double(rng)
+
+rng.intDouble(rng)
+
+rng.doubleInt(rng)
+
+rng.double3(rng)
+
+rng.ints(3)(rng)
+
+rng.int(rng)
+
+val nonneg = rng.nonNegativeEven
+
+nonneg(rng)
+
+rng.doubleElegant(rng)
+
+rng.doubleElegant2(rng)
+
+rng.randIntDouble(rng)
+
+rng.randIntDouble2(rng)
